b1d1e5d990af7b9e828fec59150968a8f8999a50
Andrew <afc1755@rit.edu>
2018-03-30 14:34:34 -0400

    can take in a URL and turn it to soup, added small dictionary file

--- scraper.py
+++ scraper.py
@@ -9,24 +9,22 @@ from bs4 import BeautifulSoup
 command line webscraping, main center for project code
 """
 
-def main():
-    #geneName = input("Please enter a url")
-    page = 'http://www.bloomberg.com/quote/INDU:IND'
+def handleURL(geneURL):
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    htmlPart = http.request('GET', page)
-    #print(htmlPart)
+    htmlPart = http.request('GET', geneURL)
+    keywordArray = [];
+    potentialNamesArr = [];
     soup = BeautifulSoup(htmlPart.data, "html.parser")
-    #print(soup)
-    name_box = soup.find('h1', attrs = {'class': 'name'})
-    #print(name_box)
-    name = name_box.text.strip()
-    print(name)
-    price_box = soup.find('div', attrs = {'class': 'price'})
-    price = price_box.text
-    print(price)
-    """Entrez.email = "afc1755@rit.edu"
-    with Entrez.efetch(db="gene", rettype="fasta", retmode="text", id="6273291") as handle:
-        seq_record = SeqIO.read(handle, "fasta")
-    print("%s with %i features" % (seq_record.id, len(seq_record.features)))
+    """if "gene called" in str(soup):
+        splitHTML = str(soup).split("gene called")
+        potGeneName = splitHTML[1].split()[0]
     """
+    #if "gene is called" in soup:
+
+    #print("Best Guess for Gene beign talked about:" + geneName)
+
+def main():
+    geneURL = input("Please enter a url: ")
+    handleURL(geneURL)
+
 main()
